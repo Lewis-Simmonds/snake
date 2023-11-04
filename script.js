40b24d7518c999717bff8a2e1b7171b417a9fe00
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-11-04 19:19:28 +0000

    Add snake movement

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const snakeBorder = 'darkgreen';
 //create snake
 let snake = [ {x: 300, y: 300}, {x: 285, y: 300}, {x: 270, y: 300}, {x: 255, y: 300}, {x: 240, y: 300}];
 
+let dx = 15;
+let dy = 0;
+
 //function to draw each part of the snake
 function drawSnakeBlock(snakeBlock) {
     boardCTX.fillStyle = snakeColor;
@@ -32,10 +35,21 @@ function clearCanvas() {
     boardCTX.strokeRect(0, 0, board.width, board.height);
 };
 
+//function to move shake
+function moveSnake() {
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    snake.unshift(head);
+    snake.pop();
+};
+
 //this function will loop to keep game running
 function main() {
-    clearCanvas();
-    drawSnake();
+    setTimeout(function onTick() {
+        clearCanvas();
+        moveSnake();
+        drawSnake();
+        main();
+    }, 100);
 };
 
 //start game
